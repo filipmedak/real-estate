ffa6e56b8f76f998a8f179d0c820e291353cc4ae
Filip Medak <filip.medak213@gmail.com>
2019-10-12 14:37:01 +0200

    Removed old navigation, new in creation.

--- js/rwdMenu.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function responsiveNavigation() {
-  var x = document.getElementById("rwdNav");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav";
-  }
-}
