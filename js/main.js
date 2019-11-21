ec0bb29fee8ccffdfe76d27970d3b3a4a7d5980d
Filip Medak <filip.medak213@gmail.com>
2019-11-21 19:32:35 +0100

    Real estates search sub-site added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,65 @@
-const backToTopButton = document.querySelector("#backToTop");
+//-------------------------------------------------------------------------------------------------------------------
+//Search engine autocorrect
 
-window.addEventListener("scroll", scrollFunction);
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+let cities;
 
-function scrollFunction(){
-    if(window.pageYOffset > 1200){
-        backToTopButton.style.display = "block";
-    }
-    else{
-        backToTopButton.style.display = "none";
-    }
+// Get cities
+const getCity = async () => {
+        const res = await fetch('../data/cities.json');
+        cities = await res.json();
+};
+
+// FIlter cities
+const searchCities = searchText => {
+ // Get matches to current text input
+ let matches = cities.filter(city => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return city.name.match(regex) || city.abbr.match(regex);
+       });
+
+        // Clear when input or matches are empty
+        if (searchText.length === 0) {
+        matches = [];
+        matchList.innerHTML = '';
+        }
+
+        outputHtml(matches);
+};
+
+// Show results in HTML
+const outputHtml = matches => {
+ if (matches.length > 0) {
+  const html = matches
+   .map(
+    match => `<div class="card card-body mb-1 p-2">
+            <h4><a href="#">${match.name} (${match.abbr})</a></h4>
+            </div>`
+   )
+   .join('');
+  matchList.innerHTML = html;
+ }
+};
+
+window.addEventListener('DOMContentLoaded', getCity);
+search.addEventListener('input', () => searchCities(search.value));
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//Hide language sticky element
+let previousScrollPosition = window.pageYOffset;
+const items = document.getElementById("sticky");
+
+window.onscroll = function() {
+        let currentScrollPosition = window.pageYOffset;
+        if (previousScrollPosition > currentScrollPosition){
+                items.style.opacity = "1";
+        }
+        else{
+                items.style.opacity = "0";
+                items.style.transitionDuration = "1.25s";
+        }
+        previousScrollPosition = currentScrollPosition;
 }
+//-------------------------------------------------------------------------------------------------------------------
