0f14a5bd0f97761ea39796fa7de322e2a9fb96ee
DanielLovrinov <56223191+DanielLovrinov@users.noreply.github.com>
2020-01-30 14:30:26 +0100

    Admin pannel - promjene,dodavanja

--- includes/adminInc/content.php
+++ includes/adminInc/content.php
@@ -1,61 +1,132 @@
 <?php
-if (isset($_POST["submit"])) {
+// if (isset($_POST["submit"])) {
 
-    $type=trim($_POST["type"]);
-    $location=trim($_POST["location"]);
-    $price=trim($_POST["price"]);
-    $rooms=trim($_POST["rooms"]);
-    $size=trim($_POST["size"]);
+//     $type=trim($_POST["type"]);
+//     $location=trim($_POST["location"]);
+//     $price=trim($_POST["price"]);
+//     $rooms=trim($_POST["rooms"]);
+//     $size=trim($_POST["size"]);
 
-    $type = mysqli_real_escape_string($connection, $type);
-    $location = mysqli_real_escape_string($connection, $location);
-    $price = mysqli_real_escape_string($connection, $price);
-    $rooms = mysqli_real_escape_string($connection, $rooms);
-    $size = mysqli_real_escape_string($connection, $size);
+//     $type = mysqli_real_escape_string($connection, $type);
+//     $location = mysqli_real_escape_string($connection, $location);
+//     $price = mysqli_real_escape_string($connection, $price);
+//     $rooms = mysqli_real_escape_string($connection, $rooms);
+//     $size = mysqli_real_escape_string($connection, $size);
 
-    $sql = "SELECT id FROM estates ORDER BY id DESC LIMIT 1";
-    $result = $connection->query($sql);
-    $row = $result->fetch_assoc();
-    $id=$row["id"]+1;
+//     $sql = "SELECT id FROM estates ORDER BY id DESC LIMIT 1";
+//     $result = $connection->query($sql);
+//     $row = $result->fetch_assoc();
+//     $id=$row["id"]+1;
 
-    $target_dir = "../../img/estates/'.$id.'/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-
-
-$sql = "INSERT INTO estates (type, location, price, rooms, size)
-        VALUES ('$type', '$location', '$price', '$rooms', '$size')";
+//     $target_dir = "../../img/estates/'.$id.'/";
+//     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+//     $uploadOk = 1;
+//     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 
-if (!mysqli_query($connection,$sql)) {
-    die('Error: ' . mysqli_error($connection));
-  }
-  echo '<p class="text-white bg-success p-3">1 record successfuly added</p>';
+
+// $sql = "INSERT INTO estates (type, location, price, rooms, size)
+//         VALUES ('$type', '$location', '$price', '$rooms', '$size')";
+
+
+// if (!mysqli_query($connection,$sql)) {
+//     die('Error: ' . mysqli_error($connection));
+//   }
+//   echo '<p class="text-white bg-success p-3">1 record successfuly added</p>';
   
-  mysqli_close($connection);
-}
+//   mysqli_close($connection);
+// }
 
 ?>
 
-<form class="content-form py-5" action="" method="post">
-    <h2 class="mb-4">Add a real estate</h2>
-    <div>
-    <div class="my-2 px-4"><span>Type of a real estate:</span>
-    <select name="type">
-    <option value="house">House</option>
-    <option value="flat">Flat</option>
-    <option value="apartment">Apartment</option>
-    <option value="cottage">Cottage</option>
-    </select></div>
-    <br>
-    <div class="my-2 px-4"><span>Location: </span><input name="location" type="text"></div>
-    <div class="my-2 px-4"><span>Price in €: </span><input name="price" type="number"></div>
-    <div class="my-2 px-4"><span>Size(m²): </span><input name="size" type="number"></div>
-    <div class="my-2 px-4"><span>Number of rooms: </span><input name="rooms" type="number"></div>
-    <div class="my-2 px-4"><span>Image: </span><input class="imgUpload" type="file" name="fileToUpload" id="fileToUpload"></div>
-    </div>
-
-    <button class="bg-lightblue text-white border-none mt-3 pl-5 pr-5" type="submit" value="Add" name="submit">Submit</button>
-</form>
+<div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <form class="form-horizontal">
+                                <div class="card-body">
+                                    <h4 class="card-title">Ad an estate</h4>
+                                    <div class="form-group row">
+                                        <label for="fname" class="col-sm-3 text-right control-label col-form-label">City</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="fname" placeholder="City Name Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Estate type</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="lname" placeholder="Estate Type Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Price</label>
+                                        <div class="col-sm-9">
+                                            <input type="number" class="form-control" id="lname" placeholder="Price Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Number of rooms</label>
+                                        <div class="col-sm-9">
+                                            <input type="number" class="form-control" id="lname" placeholder="Rooms Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="email1" class="col-sm-3 text-right control-label col-form-label">Company</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="email1" placeholder="Company Name Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Contact No</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" id="cono1" placeholder="Contact No Here">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                    <label class="col-sm-3 text-right">Optional info</label>
+                                    <div class="col-md-9">
+                                        <div class="custom-control custom-checkbox mr-sm-2">
+                                            <input type="checkbox" class="custom-control-input" id="customControlAutosizing1">
+                                            <label class="custom-control-label" for="customControlAutosizing1">Parking</label>
+                                        </div>
+                                        <div class="custom-control custom-checkbox mr-sm-2">
+                                            <input type="checkbox" class="custom-control-input" id="customControlAutosizing3">
+                                            <label class="custom-control-label" for="customControlAutosizing3">Garage</label>
+                                        </div>
+                                        <div class="custom-control custom-checkbox mr-sm-2">
+                                            <input type="checkbox" class="custom-control-input" id="customControlAutosizing2">
+                                            <label class="custom-control-label" for="customControlAutosizing2">Lift</label>
+                                        </div>
+                                        <div class="custom-control custom-checkbox mr-sm-2">
+                                            <input type="checkbox" class="custom-control-input" id="customControlAutosizing3">
+                                            <label class="custom-control-label" for="customControlAutosizing3">Barrier free access</label>
+                                        </div>
+                                        <div class="custom-control custom-checkbox mr-sm-2">
+                                            <input type="checkbox" class="custom-control-input" id="customControlAutosizing3">
+                                            <label class="custom-control-label" for="customControlAutosizing3">Internet</label>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Description</label>
+                                        <div class="col-sm-9">
+                                            <textarea class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                    <label class="col-sm-3 text-right">Image Upload</label>
+                                    <div class="col-md-9">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                                            <label class="custom-file-label" for="validatedCustomFile">Choose image...</label>
+                                            <div class="invalid-feedback">Example invalid custom file feedback</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="border-top">
+                                    <div class="card-body">
+                                        <button type="button" class="btn btn-primary">Post estate</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
--- includes/adminInc/users.php
+++ includes/adminInc/users.php
@@ -1,18 +1,72 @@
-<?php
 
-echo 'users';
+
+<div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Basic Datatable</h5>
+                                <div class="table-responsive">
+                                    <table id="zero_config" class="table table-striped table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Id</th>
+                                                <th>First name</th>
+                                                <th>Last name</th>
+                                                <th>E-mail</th>
+                                                <th>Rights</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        <?php
 
 $sql = "SELECT * FROM users";
-$result = $connection->query($sql);
+$result = mysqli_query($connection, $sql);
 
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
+if (mysqli_num_rows($result) > 0) {
+    $counter=1;
+    while($row = mysqli_fetch_assoc($result)) {
         
-    }
+if ($row["isadmin"]==0) {
+    $admin="User";
+}else{
+    $admin="Admin";
 }
-else {
-    echo '<p>There are currently no users!</p>';
+
+echo '
+
+    <tr>
+        <td>'.$counter.'</td>
+        <td>'.$row["id"].'</td>
+        <td>'.ucfirst($row["firstname"]).'</td>
+        <td>'.ucfirst($row["lastname"]).'</td>
+        <td>'.$row["email"].'</td>
+        <td>'.$admin.'</td>
+    </tr>
+
+';
+    
+$counter++;
+
+    }
+} else {
+    echo "0 results";
 }
 
 ?>
+
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Id</th>
+                                                <th>First name</th>
+                                                <th>Last name</th>
+                                                <th>E-mail</th>
+                                                <th>Rights</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+
+                            </div>
+                        </div>
