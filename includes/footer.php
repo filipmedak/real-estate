eb5256a79569b63428e56e3e5f5e7533177af504
Filip Medak <filip.medak213@gmail.com>
2020-01-30 09:24:22 +0100

    MultiValue slider

--- includes/footer.php
+++ includes/footer.php
@@ -41,6 +41,8 @@ echo '
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
       <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
       <script src="js/main.js"></script>
+      <script src="../../js/main.js"></script>
+      <script src="../../js/multiValueSlider.js"></script>
       <script>
           AOS.init();
       </script>
